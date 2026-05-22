e65990e0e8643238627f7d74d85494cf5a0a8f85
Jayasuriya R <suryaabi3801@gmail.com>
2026-05-22 19:26:31 +0530

    email alert for price drop

---
+++ app/api/cron/check-prices/route.js
@@ -0,0 +1,85 @@
+import { sendEmailAlert } from "@/lib/email";
+import { createClient } from "@/utils/supabase/server";
+import { parse } from "next/dist/build/swc";
+import { NextResponse } from "next/server";
+
+export async function GET(){
+    return NextResponse.json({message:'Cron job works!'})
+}
+
+export async function POST(request){
+    try {
+        const authHeader = request.headers.get('authorization');
+        const cronSecret = process.env.CRON_SECRET;
+
+        if(!cronSecret || authHeader !== `Bearer ${cronSecret}`){
+            return NextResponse.json({error:'Unauthorized'}, {status:401})
+        }
+        
+        const supabase = createClient(
+            process.env.NEXT_PUBLIC_SUPABASE_URL,
+            process.env.NEXT_PUBLIC_SUPABASE_SERVICE_ROLE_KEY
+        )
+
+        const {data: products, error: productsError} = await supabase
+            .from('products')
+            .select('*')
+
+        if(productsError) throw productsError
+
+        const results = {
+            total: products.length,
+            updated: 0,
+            failed: 0,
+            changed:0
+        }
+
+        for (const product of products){
+            try {
+                const productData = await scrpeProduct(product.url);
+
+                if(!productData.currentPrice){
+                    results.failed++;
+                    continue;
+                }
+
+                const newPrice  = parseFloat(productData.currentPrice);
+                const oldPrice = parseFloat(product.current_price);
+
+                await supabase.from('products').update({
+                    current_price: newPrice,
+                    currency: productData.currencyCode || product.currency,
+                    name: productData.productName || product.name,
+                    image_url: productData.productImageUrl || product.image_url,
+                    updated_at: new Date().toISOString(),
+                }).eq('id', product.id);
+
+                if(newPrice !== oldPrice){
+                    await supabase.from('price_history').insert({
+                        product_id:product.id,
+                        price: newPrice,
+                        currency: productData.currencyCode || product.currency,
+                    });
+                   results.changed++; 
+
+                   if(newPrice < oldPrice){
+                    const {data:{user}} = await supabase.auth.admin.getUserById(product.user_id);
+                    if(user?.email){
+                      const emailResult = await sendEmailAlert(user.email, product, oldPrice, newPrice);
+                    }
+                   }
+                }
+                results.updated++;
+                return NextResponse.json({success:true,message:'Prices checked successfully', results})
+
+            } catch (error) {
+                console.error(`Failed to update product ${product.id}:`, error);
+
+            }
+        }
+
+    } catch (error) {
+        console.error('Error occurred while checking prices:', error);
+        results.failed++;
+    }
+}
